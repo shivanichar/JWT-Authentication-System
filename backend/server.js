71d199f4ab53d53273b82ab8e19c80f2708a6f10
shivani <shivani@jangra.local>
2026-07-08 13:57:39 +0530

    password hashing using bcrypt

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
 const jwt_secret = "mySuperSecretKey"
@@ -9,12 +10,14 @@ const app = express();
 
 const verifyToken = require('./middleware/authMiddleware');
 
-const user = {
-    id: 1,
-    name: 'Shivani',
-    email: "shivani@gmail.com",
-    password: "hello"
-} 
+const users = [];
+
+// const user = {
+//     id: 1,
+//     name: 'Shivani',
+//     email: "shivani@gmail.com",
+//     password: "hello"
+// } 
 
 app.use(cors())
 app.use(express.json())
@@ -24,29 +27,74 @@ app.get('/', (req,res) => {
     res.send('Backend running!!!')
 })
 
+app.post("/register", async (req, res) => {
+    const {name, password, email} = req.body;
+
+    const existingUser = users.find(
+        user => users.email === email
+    );
+
+    if(existingUser){
+        return res.status(409).json({
+            message: "user already exists"
+        })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = {
+        id: users.length + 1,
+        name,
+        email,
+        password: hashedPassword
+    }
+    users.push(newUser);
+
+    console.log(users)
+
+    return res.status(201).json({
+        message: "User Registered successfully"
+    })
+})
+
 app.post('/login', (req,res) => {
     console.log("Login hit")
     const {email, password} = req.body
-    if(email == user.email && password == user.password){
-        const token = jwt.sign(
-            {
-                id: user.id,
-                email: user.email,
-                name: user.name
-            },
-            jwt_secret,
-            {
-                expiresIn: "1m"
-            }
-        )
-        return res.json({
-            message: 'Login successful',
-            token
+
+    const user = users.find(
+        user => user.email === email
+    );
+
+    if(!user){
+        return res.status(401).json({
+           message: 'Invalid credentials!'
         })
     }
-    res.status(401).json({
-        message: 'Invalid credentials!'
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+    if(!password){
+        return res.status(401).json({
+            message: 'Invalid credentials!'
+        })
+    }
+
+    
+    const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email,
+            name: user.name
+        },
+        jwt_secret,
+        {
+            expiresIn: "1m"
+        }
+    )
+    return res.json({
+        message: 'Login successful',
+        token
     })
+    
 })
 
 
