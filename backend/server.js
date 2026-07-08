25f51d794d019bf38933084a14bbfa52c44d0573
shivani <shivani@jangra.local>
2026-07-08 17:54:23 +0530

    Added MongoDB Backend

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
+require("dotenv").config()
+const connectDb = require("./config/db")
 const express = require("express")
 const cors = require("cors")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
-const jwt_secret = "mySuperSecretKey"
+const jwt_secret = process.env.JWT_SECRET
 
+
+connectDb();
 
 const app = express();
 
 const verifyToken = require('./middleware/authMiddleware');
 
-const users = [];
+const User = require("./models/User")
 
-// const user = {
-//     id: 1,
-//     name: 'Shivani',
-//     email: "shivani@gmail.com",
-//     password: "hello"
-// } 
 
 app.use(cors())
 app.use(express.json())
@@ -28,72 +26,90 @@ app.get('/', (req,res) => {
 })
 
 app.post("/register", async (req, res) => {
-    const {name, password, email} = req.body;
+    try{
+        const {name, password, email} = req.body;
 
-    const existingUser = users.find(
-        user => user.email === email
-    );
+        if(!email || !name || !password){
+            return res.status(400).json({
+                message: "All fields are required!"
+            })
+        }
 
-    if(existingUser){
-        return res.status(409).json({
-            message: "user already exists"
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            return res.status(409).json({
+                message: "user already exists"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        await newUser.save();
+
+        return res.status(201).json({
+            message: "User Registered successfully"
         })
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = {
-        id: users.length + 1,
-        name,
-        email,
-        password: hashedPassword
-    }
-    users.push(newUser);
-
-    console.log(users)
-
-    return res.status(201).json({
-        message: "User Registered successfully"
-    })
+    } catch(err){
+        return res.status(500).json({
+            message: "Internal Server error!"
+        })
+    };
 })
 
 app.post('/login', async (req,res) => {
     console.log("Login hit")
-    const {email, password} = req.body
+    try{
+        const {email, password} = req.body
 
-    const user = users.find(
-        user => user.email === email
-    );
+        if(!email || !password){
+            return res.status(400).jason({
+                message: "Both Email & Password is required!"
+            })
+        }
 
-    if(!user){
-        return res.status(401).json({
-           message: 'Invalid credentials!'
-        })
-    }
-    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        const user = await User.findOne({email});
 
-    if(!isPasswordCorrect){
-        return res.status(401).json({
+        if(!user){
+            return res.status(401).json({
             message: 'Invalid credentials!'
-        })
-    }
+            })
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+
+        if(!isPasswordCorrect){
+            return res.status(401).json({
+                message: 'Invalid credentials!'
+            })
+        }
 
     
-    const token = jwt.sign(
-        {
-            id: user.id,
-            email: user.email,
-            name: user.name
-        },
-        jwt_secret,
-        {
-            expiresIn: "1m"
-        }
-    )
-    return res.json({
-        message: 'Login successful',
-        token
-    })
+        const token = jwt.sign(
+            {
+                id: user._id,
+                email: user.email,
+                name: user.name
+            },
+            jwt_secret,
+            {
+                expiresIn: "1m"
+            }
+        )
+        return res.json({
+            message: 'Login successful',
+            token
+        })
+    } catch(err){
+        return res.status(500).json({
+            message: "internal Server Error!"
+        })
+    }
     
 })
 
@@ -101,11 +117,7 @@ app.post('/login', async (req,res) => {
 app.get("/profile", verifyToken, (req,res) => {
     res.json({
         message: "Welcome to the Profile",
-        user: {
-            id: '1',
-            name: 'Shivani',
-            email: 'shivani@gmail.com'
-        }
+        user: req.user
     })
 })
 
